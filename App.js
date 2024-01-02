3558bd2b7d92a8ceb2084632314c8420196d8e28
Luke Colombo <123432468+lukecolombo03@users.noreply.github.com>
2024-01-02 14:26:14 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -2,90 +2,105 @@ import './App.css';
 import {useState} from "react";
 
 //TODO:
-//      -User input to choose teams
-//      -Render a bunch of different matchups
-//      -change conditional statement in style
+//      -Store matchups as arrays within an array
+//      -Have a "next matchup" button
+//      -Format matchups automatically after user input
+//      -Multiple rounds: after declaring winners of all matches, make new matches w/ those winners
 function App() {
     return (
-        <div className="App">
+        <>
             <h1>Welcome to my app</h1>
-            <ButtonList/>
-        </div>
+            <Bracket/>
+        </>
     );
 }
 
+//Array of matchups
+//A single matchup is an array of two teams (represented as objects)
+//TODO: handle empty arrays
 let initialTeams = [
-    {name: "Oklahoma", id: 1, votes: 0},
-    {name: "California", id: 2, votes: 0},
-    {name: "Oregon", id: 3, votes: 0},
-    {name: "Hawaii", id: 4, votes: 0},
-    {name: "Ohio", id: 5, votes: 0},
-    {name: "Nebraska", id: 6, votes: 0},
-    {name: "Georgia", id: 7, votes: 0},
-    {name: "Texas", id: 8, votes: 0},
+    [{name: "Jake", id: 0, votes: 0}, {name: "Cole", id: 1, votes: 0}],
+    [{name: "Aidan", id: 0, votes: 0}, {name: "Zach", id: 1, votes: 0}]
 ];
 
-function ButtonList() {
-    const [teams, setTeams] = useState(initialTeams);
+function Bracket() {
+    const [matchups, setMatchups] = useState(initialTeams);
 
-    //teams is a list of dictionaries, so this must return a dictionary
+
     function handleClick(index) {
-        const nextTeams = teams.map((team, i) => {
-            if (i === index) {
-                team.votes++;
-                return team;
-            } else {
-                return team;
-            }
-        });
-        setTeams(nextTeams);
+        let nextMatchups = []
+        for (let match of matchups) {
+            nextMatchups.push(match.map((team, i) => {
+                if (i === index) {
+                    team.votes++;
+                    return team;
+                } else {
+                    return team;
+                }
+            }));
+        }
+        console.log(nextMatchups);
+        setMatchups(nextMatchups);
     }
 
     //Add a new team based on data from child component (AddNewTeam)
-    function handleNewTeam(childData) {
-        const newTeam = {name: childData, votes: 0, id: teams[teams.length - 1].id + 1};
-        const nextTeams = [teams, newTeam];
-        setTeams(nextTeams);
-        for (const team in teams) {
-            console.log(team.name)
+    // function handleNewTeam(childData) {
+    //     let newID = (teams.length === 0) ? 0 : teams[teams.length - 1].id + 1;
+    //     const newTeam = {name: childData, id: newID, votes: 0};
+    //     const nextTeams = [...teams, newTeam];
+    //     setTeams(nextTeams);
+    // }
+    let winnerDisplay;
+    let teamDisplay;
+    //Takes in 2 teams (objects) and returns a String representing the winner
+    //NOTE: does not work if it returns an object
+    //NOTE: right now it's hardcoded so that a match only works with 2 teams
+    function getWinner(team1, team2) {
+        if (team1.votes === team2.votes) {
+            return "It's a tie!";
+        } else if (team1.votes > team2.votes) {
+            return team1.name;
+        } else {
+            return team2.name;
         }
     }
+    //outer map loops through each match (list of 2 teams) in matchups
+    //inner map loops through each team in that match
 
-    const mostClicked = Math.max.apply(null, teams.map(team => team.votes));
+    //another way to get winner is to loop through each match in matchups,
+    // get the highest vote count in each, then compare each team to the highest vote count
+    // in its own match
+    teamDisplay = matchups.map((match, i) => (
+        <>
+            {match.map((team, i) => (
+                <>
+                    <li key={i}>
+                        <button onClick={() => handleClick(i)}>
+                            {match[i].name} votes: {match[i].votes}
+                        </button>
+                    </li>
 
-    function isWinner(team) {
-        return team.votes === mostClicked;
-    }
-
-    let winners = teams.filter(isWinner);
-    let winnerDisplay;
-    if (winners.length > 1) {
-        winnerDisplay = "It's a tie!"
-    } else {
-        winnerDisplay = <ul>{winners.map((team, i) => (
-            <li key={i}>{team.name}</li>))}</ul>
-    }
+                </>
+            ))}
+            <h3>
+                Winner: {getWinner(match[0], match[1])}
+            </h3>
+        </>
+    ))
 
     return (<>
-        <h3>{teams[0].name} VS. {teams[1].name}</h3>
-        <ul>
-            {teams.map((team, i) => (
-                <li key={i}>
-                    <button onClick={() => handleClick(i)}
-                            style={{
-                                color: teams[i].votes === mostClicked ? 'blue' : 'gray'
-                            }}> {teams[i].name} votes: {teams[i].votes}</button>
-                </li>))} </ul>
-        <h3>Winner(s): {winnerDisplay}</h3>
-        <AddNewTeam parentCallback={handleNewTeam}/>
+        <ul> {teamDisplay} </ul>
+        {/*<NewTeamForm parentCallback={handleNewTeam}/>*/}
     </>);
 }
 
-function AddNewTeam({parentCallback}) {
+function NewTeamForm({parentCallback}) {
     const [state, setState] = useState('')
 
-    function handleSubmit() {
+    function handleSubmit(event) {
+        event.preventDefault();
         parentCallback(state);
+        setState('')
     }
 
     return <form onSubmit={handleSubmit}>
